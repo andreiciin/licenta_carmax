1faa5c8a1e45f0008a9b7155f2d11f4eca4b2aa1
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-04-13 01:11:40 +0300

    commit-17: category form + parent/children list

--- carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/category/CategoryService.java
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/category/CategoryService.java
@@ -5,7 +5,9 @@ import com.carmaxbackend.admin.category.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CategoryService {
@@ -14,5 +16,45 @@ public class CategoryService {
 
 	public List<Category> listAll() {
 		return (List<Category>) repo.findAll();
+	}
+
+	public List<Category> listCategoriesUsedInForm() {
+		List<Category> categoriesUsedInForm = new ArrayList<>();
+
+		Iterable<Category> categoriesInDB = repo.findAll();
+
+		for (Category category : categoriesInDB) {
+			if (category.getParent() == null) {
+				categoriesUsedInForm.add(new Category(category.getName()));
+
+				Set<Category> children = category.getChildren();
+
+				for (Category subCategory : children) {
+					String name = "--" + subCategory.getName();
+					categoriesUsedInForm.add(new Category(name));
+
+					listChildren(categoriesUsedInForm, subCategory, 1);
+				}
+			}
+		}
+
+		return categoriesUsedInForm;
+	}
+
+	private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+		int newSubLevel = subLevel + 1;
+		Set<Category> children = parent.getChildren();
+
+		for (Category subCategory : children) {
+			String name = "";
+			for (int i = 0; i < newSubLevel; i++) {
+				name += "--";
+			}
+			name += subCategory.getName();
+
+			categoriesUsedInForm.add(new Category(name));
+
+			listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+		}
 	}
 }
