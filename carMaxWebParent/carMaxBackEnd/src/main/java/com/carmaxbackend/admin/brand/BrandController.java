c96666a7cc78cf8c719ad6792205b9260ac29824
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-04-16 15:27:17 +0300

    commit-23: brand form

--- carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/brand/BrandController.java
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/brand/BrandController.java
@@ -1,6 +1,8 @@
 package com.carmaxbackend.admin.brand;
 
 import com.carmax.common.entity.Brand;
+import com.carmax.common.entity.Category;
+import com.carmaxbackend.admin.category.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,13 +13,27 @@ import java.util.List;
 @Controller
 public class BrandController {
 	@Autowired
-	private BrandService service;
+	private BrandService brandService;
+
+	@Autowired
+	private CategoryService categoryService;
 
 	@GetMapping("/brands")
 	public String listAll(Model model) {
-		List<Brand> listBrands = service.listAll();
+		List<Brand> listBrands = brandService.listAll();
 		model.addAttribute("listBrands", listBrands);
 
 		return "brands/brands";
+	}
+
+	@GetMapping("/brands/new")
+	public String newBrand(Model model) {
+		List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+
+		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("brand", new Brand());
+		model.addAttribute("pageTitle", "Create New Brand");
+
+		return "brands/brand_form";
 	}
 }
