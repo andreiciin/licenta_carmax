0a035835492a8d6c8227ceadb61ec1faecf3ae98
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-04-18 00:45:54 +0300

    commit-26: product entity

---
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/product/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.carmaxbackend.admin.product;
+
+import com.carmax.common.entity.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer>, PagingAndSortingRepository<Product, Integer> {
+}
