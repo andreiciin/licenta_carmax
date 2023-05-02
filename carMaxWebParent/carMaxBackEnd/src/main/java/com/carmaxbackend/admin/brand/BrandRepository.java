9680ae94c5c8c31f282e178741ba8997d946ed67
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-05-02 23:48:59 +0300

    shelf-36: brand uniqueness

--- carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/brand/BrandRepository.java
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/brand/BrandRepository.java
@@ -1,9 +1,17 @@
 package com.carmaxbackend.admin.brand;
 
 import com.carmax.common.entity.Brand;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface BrandRepository extends CrudRepository<Brand, Integer>, PagingAndSortingRepository<Brand, Integer> {
 	public Long countById(Integer id);
+
+	public Brand findByName(String name);
+
+	@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+	public Page<Brand> findAll(String keyword, Pageable pageable);
 }
