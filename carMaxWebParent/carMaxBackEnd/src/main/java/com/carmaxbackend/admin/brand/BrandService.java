a5c23fda0233be854daa6647289531c18552716f
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-05-03 00:41:13 +0300

    shelf-37: brand sort, pagination and filter

--- carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/brand/BrandService.java
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/brand/BrandService.java
@@ -2,6 +2,10 @@ package com.carmaxbackend.admin.brand;
 
 import com.carmax.common.entity.Brand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,11 +13,26 @@ import java.util.NoSuchElementException;
 
 @Service
 public class BrandService {
+	public static final int BRANDS_PER_PAGE = 10;
 	@Autowired
 	private BrandRepository repo;
 
 	public List<Brand> listAll() {
 		return (List<Brand>) repo.findAll();
+	}
+
+	public Page<Brand> listByPage(int pageNum, String sortField, String sortDir, String keyword) {
+		Sort sort = Sort.by(sortField);
+
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+
+		Pageable pageable = PageRequest.of(pageNum - 1, BRANDS_PER_PAGE, sort);
+
+		if (keyword != null) {
+			return repo.findAll(keyword, pageable);
+		}
+
+		return repo.findAll(pageable);
 	}
 
 	public Brand save(Brand brand) {
