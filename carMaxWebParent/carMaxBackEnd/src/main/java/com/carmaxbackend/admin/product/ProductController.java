c65a83c3cbae3b02cc52e4afe645cb61fe95cedc
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-06-01 00:40:25 +0300

    commit-46: product save

--- carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/product/ProductController.java
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/product/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -41,10 +42,9 @@ public class ProductController {
 	}
 
 	@PostMapping("/products/save")
-	public String saveProduct(Product product) {
-		System.out.println("Product Name: " + product.getName());
-		System.out.println("Brand ID: " + product.getBrand().getId());
-		System.out.println("Category ID: " + product.getCategory().getId());
+	public String saveProduct(Product product, RedirectAttributes ra) {
+		productService.save(product);
+		ra.addFlashAttribute("message", "The product has been saved successfully.");
 
 		return "redirect:/products";
 	}
--- carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/product/ProductService.java
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/product/ProductService.java
@@ -4,6 +4,7 @@ import com.carmax.common.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,5 +15,22 @@ public class ProductService {
 
 	public List<Product> listAll() {
 		return (List<Product>) repo.findAll();
+	}
+
+	public Product save(Product product) {
+		if (product.getId() == null) {
+			product.setCreatedTime(new Date());
+		}
+
+		if (product.getAlias() == null || product.getAlias().isEmpty()) {
+			String defaultAlias = product.getName().replaceAll(" ", "-");
+			product.setAlias(defaultAlias);
+		} else {
+			product.setAlias(product.getAlias().replaceAll(" ", "-"));
+		}
+
+		product.setUpdatedTime(new Date());
+
+		return repo.save(product);
 	}
 }
