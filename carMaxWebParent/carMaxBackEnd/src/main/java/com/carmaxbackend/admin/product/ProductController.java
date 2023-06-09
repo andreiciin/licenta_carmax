3f46a8717fdf1c154f36445f36bc447a5607df77
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-06-10 00:02:49 +0300

    commit-51: add product main image

--- carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/product/ProductController.java
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/product/ProductController.java
@@ -2,15 +2,20 @@ package com.carmaxbackend.admin.product;
 
 import com.carmax.common.entity.Brand;
 import com.carmax.common.entity.Product;
+import com.carmaxbackend.admin.FileUploadUtil;
 import com.carmaxbackend.admin.brand.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -43,8 +48,23 @@ public class ProductController {
 	}
 
 	@PostMapping("/products/save")
-	public String saveProduct(Product product, RedirectAttributes ra) {
-		productService.save(product);
+	public String saveProduct(Product product, RedirectAttributes ra,
+							  @RequestParam("fileImage") MultipartFile multipartFile) throws IOException {
+
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			product.setMainImage(fileName);
+
+			Product savedProduct = productService.save(product);
+			String uploadDir = "../product-images/" + savedProduct.getId();
+
+			FileUploadUtil.cleanDir(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+
+		} else {
+			productService.save(product);
+		}
+
 		ra.addFlashAttribute("message", "The product has been saved successfully.");
 
 		return "redirect:/products";
