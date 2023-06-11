f2c5b5c6dc6b2c09cea1b09664103e15c2c361fb
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-06-12 00:35:50 +0300

    commit-57: pagination, sort, filter products

--- carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/product/ProductRepository.java
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/product/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.carmaxbackend.admin.product;
 
 import com.carmax.common.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -15,4 +17,11 @@ public interface ProductRepository extends CrudRepository<Product, Integer>, Pag
 	public void updateEnabledStatus(Integer id, boolean enabled);
 
 	public Long countById(Integer id);
+
+	@Query("SELECT p FROM Product p WHERE p.name LIKE %?1% "
+			+ "OR p.shortDescription LIKE %?1% "
+			+ "OR p.fullDescription LIKE %?1% "
+			+ "OR p.brand.name LIKE %?1% "
+			+ "OR p.category.name LIKE %?1%")
+	public Page<Product> findAll(String keyword, Pageable pageable);
 }
