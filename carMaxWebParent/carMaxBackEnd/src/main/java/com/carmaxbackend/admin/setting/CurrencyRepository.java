ba5b38ce6fcb5760a21e28d1e0bed5ae9a939fbb
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-06-14 01:28:59 +0300

    commit-70: currency entity and repo

---
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/setting/CurrencyRepository.java
@@ -0,0 +1,8 @@
+package com.carmaxbackend.admin.setting;
+
+import com.carmax.common.entity.Currency;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CurrencyRepository extends CrudRepository<Currency, Integer> {
+
+}
