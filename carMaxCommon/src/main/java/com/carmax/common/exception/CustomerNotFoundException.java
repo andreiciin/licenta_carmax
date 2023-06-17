1e13be960dbc9298ca6dd1c6683713708f8da714
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-06-18 02:26:55 +0300

    commit-85: all customer page admin related

---
+++ carMaxCommon/src/main/java/com/carmax/common/exception/CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.carmax.common.exception;
+
+public class CustomerNotFoundException extends Exception {
+
+	public CustomerNotFoundException(String message) {
+		super(message);
+	}
+
+}
---
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/customer/CustomerRestController.java
@@ -0,0 +1,22 @@
+package com.carmaxbackend.admin.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerRestController {
+	@Autowired
+	private CustomerService service;
+
+	@PostMapping("/customers/check_email")
+	public String checkDuplicateEmail(@Param("id") Integer id, @Param("email") String email) {
+		if (service.isEmailUnique(id, email)) {
+			return "OK";
+		} else {
+			return "Duplicated";
+		}
+	}
+}
+
