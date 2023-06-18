22f141e6b7e3002d886894cd29404c81843b14aa
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-06-18 12:01:31 +0300

    commit-86: customer roles fix

--- carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/customer/CustomerService.java
+++ carMaxWebParent/carMaxBackEnd/src/main/java/com/carmaxbackend/admin/customer/CustomerService.java
@@ -67,13 +67,19 @@ public class CustomerService {
 	}
 
 	public void save(Customer customerInForm) {
+		Customer customerInDB = customerRepo.findById(customerInForm.getId()).get();
+
 		if (!customerInForm.getPassword().isEmpty()) {
 			String encodedPassword = passwordEncoder.encode(customerInForm.getPassword());
 			customerInForm.setPassword(encodedPassword);
 		} else {
-			Customer customerInDB = customerRepo.findById(customerInForm.getId()).get();
 			customerInForm.setPassword(customerInDB.getPassword());
 		}
+
+		customerInForm.setEnabled(customerInDB.isEnabled());
+		customerInForm.setCreatedTime(customerInDB.getCreatedTime());
+		customerInForm.setVerificationCode(customerInDB.getVerificationCode());
+
 		customerRepo.save(customerInForm);
 	}
 
