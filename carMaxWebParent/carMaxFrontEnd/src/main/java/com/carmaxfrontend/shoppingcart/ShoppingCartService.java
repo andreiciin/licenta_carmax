fbac8edd4ec92e85076df2265f2c42a242363a20
Andrei Ciineanu <stefanciineanu@gmail.com>
2023-07-02 23:40:20 +0300

    commit-108: update product quantity in cart

--- carMaxWebParent/carMaxFrontEnd/src/main/java/com/carmaxfrontend/shoppingcart/ShoppingCartService.java
+++ carMaxWebParent/carMaxFrontEnd/src/main/java/com/carmaxfrontend/shoppingcart/ShoppingCartService.java
@@ -3,16 +3,21 @@ package com.carmaxfrontend.shoppingcart;
 import com.carmax.common.entity.CartItem;
 import com.carmax.common.entity.Customer;
 import com.carmax.common.entity.Product;
+import com.carmaxfrontend.product.ProductRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class ShoppingCartService {
 
 	@Autowired
 	private CartItemRepository cartRepo;
+	@Autowired
+	private ProductRepository productRepo;
 
 	public Integer addProduct(Integer productId, Integer quantity, Customer customer)
 			throws ShoppingCartException {
@@ -44,5 +49,12 @@ public class ShoppingCartService {
 
 	public List<CartItem> listCartItems(Customer customer) {
 		return cartRepo.findByCustomer(customer);
+	}
+
+	public float updateQuantity(Integer productId, Integer quantity, Customer customer) {
+		cartRepo.updateQuantity(quantity, customer.getId(), productId);
+		Product product = productRepo.findById(productId).get();
+		float subtotal = product.getDiscountPrice() * quantity;
+		return subtotal;
 	}
 }
